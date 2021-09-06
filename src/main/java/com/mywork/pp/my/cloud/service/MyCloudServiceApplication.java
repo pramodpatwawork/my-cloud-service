4bd6c8bfc7bc9bbf66f29e334e178a26d65aeb39
pramodpatwawork <pramod_patwa2003@yahoo.com>
2021-09-06 14:40:20 +0530

    first commit

---
+++ src/main/java/com/mywork/pp/my/cloud/service/MyCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mywork.pp.my.cloud.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCloudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mywork/pp/my/cloud/service/MyCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywork.pp.my.cloud.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
